5e4418d5ef5af641e75e2524063a1b25e87df295
Bruno Cesar <brunoc.souza@outlook.com.br>
2025-06-20 03:00:09 -0300

    Responsive role detail page

--- app/(pages)/get/roles/[id]/page.tsx
+++ app/(pages)/get/roles/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { PageProps } from "@/.next/types/app/layout";
 import { prisma } from "@/lib/prisma";
 
-export default async function Page({ params }:PageProps) {
-
+export default async function Page({ params }: PageProps) {
   const role = await prisma.role.findUnique({
     where: {
       id_role: params.id,
@@ -10,9 +9,17 @@ export default async function Page({ params }:PageProps) {
   });
 
   return (
-    <div>
-      <h1>Role selected: {role?.role_name}</h1>
-      <h2>Id: {role?.id_role}</h2>
+    <div className="min-w-full h-screen flex justify-center items-center border border-pink-500 font-sans">
+      <div className="container flex flex-col border border-white rounded w-10/12 h-2/3 md:w-6/12 lg:w-4/12 items-center justify-center font-medium break-words text-center px-4">
+        <h1>
+          Role selected:
+          <span className="text-teal-400"> {role?.role_name}</span>
+        </h1>
+        <h1>
+          Role selected:
+          <span className="text-teal-400"> {role?.id_role}</span>
+        </h1>
+      </div>
     </div>
   );
 }
