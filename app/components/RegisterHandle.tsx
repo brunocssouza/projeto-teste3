ffe7228812be24bb6be0f3480bd5f54d80e52649
Bruno Cesar <brunoc.souza@outlook.com.br>
2025-06-20 10:20:42 -0300

    Changes made

---
+++ app/components/RegisterHandle.tsx
@@ -0,0 +1,12 @@
+
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { prisma } from "@/lib/prisma";
+
+export default function Page() {
+
+
+  return (
+    <div></div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,61 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { prisma } from "@/lib/prisma";
-import RegisterForm from "./components/RegisterForm";
+"use client";
 
-export default function Page() {
-  async function POST(
-    name: string,
-    cpf: number,
-    email: string,
-    password: string
-  ) {
-    const users = await prisma.user.create({
-      data: {
-        user_name: name,
-        cpf: cpf,
-        email: email,
-        password: password,
-      },
-    });
-  }
+import { useState } from "react";
+import RegisterHandle from "./components/RegisterHandle";
 
-  return (
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+return (
+  <>
     <main className="min-w-full h-screen flex justify-center items-center border border-pink-500 font-sans bg-white">
       <div className="container flex flex-col shadow-lg border border-s-neutral-200 rounded w-10/12 h-2/3 md:w-6/12 lg:w-4/12 items-center justify-center font-medium break-words px-4 text-black">
-        <RegisterForm></RegisterForm>
-
+        <h1 className="text-center font-bold text-4xl mb-10">REGISTER</h1>
+        <form>
+          <label className="block" htmlFor="">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="border border-s-neutral-200 shadow-lg mb-5"
+            onChange={(event) => setName(event.target.value)}
+          />
+          <label htmlFor="" className="block">
+            CPF
+          </label>
+          <input
+            type="number"
+            className="border border-s-neutral-200 shadow-lg mb-5"
+            onChange={(event) => setCpf(event.target.value)}
+          />
+          <label htmlFor="" className="block">
+            E-mail
+          </label>
+          <input
+            type="email"
+            className="border border-s-neutral-200 shadow-lg mb-5"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <label htmlFor="" className="block">
+            Password
+          </label>
+          <input
+            type="password"
+            className="border border-s-neutral-200 shadow-lg mb-5"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div className="flex justify-center"></div>
+        </form>
+        <button className="block text-center shadow-lg rounded bg-yellow-500 w-full h-10">
+          Submit
+        </button>
       </div>
     </main>
-  );
+  </>
+)
 }
