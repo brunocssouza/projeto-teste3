6c8e2340fbaa8c4a2067b9d216c8d7ffab509ddc
Bruno Cesar <brunoc.souza@outlook.com.br>
2025-06-20 11:27:43 -0300

    Register screen

---
+++ app/components/RegisterHandler.tsx
@@ -0,0 +1,29 @@
+'use server'
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { prisma } from "@/lib/prisma";
+
+export default async function RegisterHandler(user: {
+  name: string;
+  cpf: number;
+  email: string;
+  password: string;
+}) {
+
+  try {
+    console.log("Registrando...");
+    await prisma.user.create({
+      data: {
+        user_name: user.name,
+        cpf: user.cpf,
+        email: user.email,
+        password: user.password,
+      },
+    });
+    console.log("Registrado");
+    return true;
+  } catch (error) {
+    console.error("Erro ao registrar:", error);
+    return false;
+  }
+}
